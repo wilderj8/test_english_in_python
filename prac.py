9042077f4a0bde9602e54d971e5bca433dfb1598
wilderj8 <90732363+wilderj8@users.noreply.github.com>
2022-07-02 19:19:31 -0500

    jul/2/2022 [1]

---
+++ prac.py
@@ -0,0 +1,12 @@
+print("Inicia")
+letra='y'
+numero=1
+while (letra=='y' or numero==1):
+    print("Escriba letra")
+    letra=input()
+    print("Escriba número")
+    numero=input()
+    print(letra)
+    print(numero)
+
+print("termina")
