8d402f046bf6ca7e109e46b1b0e1a6e434a9788b
wilderj8 <90732363+wilderj8@users.noreply.github.com>
2022-08-27 14:27:45 -0500

    27/ago/2022 [1]

---
+++ theory.py
@@ -0,0 +1,272 @@
+def main_theory():
+    abecedary()
+
+def recorrer_lista_abecedary(list_test):
+    print("correct words")
+    for x in list_test:
+        print(x)
+
+def list_correct_abecedary():
+    print()
+    print("letter--pronunciation")
+    print("A--ei")
+    print("B--bi")
+    print("C--ci")
+    print("D--di")
+    print("E--i")
+    print("F--ef")
+    print("G--yi")
+    print("H--eich")
+    print("I--ai")
+    print("J--jei")
+    print("K--kei")
+    print("L--el")
+    print("M--em")
+    print("N--en")
+    print("O--ou")
+    print("P--pi")
+    print("K--kiu")
+    print("R--er")
+    print("S--es")
+    print("T--ti")
+    print("U--iu")
+    print("V--vi")
+    print("W--dabliu")
+    print("X--ex")
+    print("Y--uai")
+    print("Z--set")
+
+
+def abecedary():
+    good=0
+    vec_abe=["------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------",]
+    print()
+    print("ABECEDARY")
+    exit="not"
+    while(exit=="not"):
+        letter_abe=input("write the letter [A] in english: ")
+        if(letter_abe=="ei"):
+            print("correct")
+            good+=1
+            vec_abe[0]="A"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [B] in english: ")
+        if(letter_abe=="bi"):
+            print("correct")
+            good+=1
+            vec_abe[1]="B"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [C] in english: ")
+        if(letter_abe=="ci"):
+            print("correct")
+            good+=1
+            vec_abe[2]="C"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [D] in english: ")
+        if(letter_abe=="di"):
+            print("correct")
+            good+=1
+            vec_abe[3]="D"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [E] in english: ")
+        if(letter_abe=="i"):
+            print("correct")
+            good+=1
+            vec_abe[4]="E"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [F] in english: ")
+        if(letter_abe=="ef"):
+            print("correct")
+            good+=1
+            vec_abe[5]="F"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [G] in english: ")
+        if(letter_abe=="yi"):
+            print("correct")
+            good+=1
+            vec_abe[6]="G"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [H] in english: ")
+        if(letter_abe=="eich"):
+            print("correct")
+            good+=1
+            vec_abe[7]="H"
+        else:
+            print("incorrect")
+        
+        letter_abe=input("write the letter [I] in english: ")
+        if(letter_abe=="ai"):
+            print("correct")
+            good+=1
+            vec_abe[8]="I"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [J] in english: ")
+        if(letter_abe=="jei"):
+            print("correct")
+            good+=1
+            vec_abe[9]="J"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [K] in english: ")
+        if(letter_abe=="kei"):
+            print("correct")
+            good+=1
+            vec_abe[10]="K"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [L] in english: ")
+        if(letter_abe=="el"):
+            print("correct")
+            good+=1
+            vec_abe[11]="L"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [M] in english: ")
+        if(letter_abe=="em"):
+            print("correct")
+            good+=1
+            vec_abe[12]="M"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [N] in english: ")
+        if(letter_abe=="en"):
+            print("correct")
+            good+=1
+            vec_abe[13]="N"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [O] in english: ")
+        if(letter_abe=="ou"):
+            print("correct")
+            good+=1
+            vec_abe[14]="O"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [P] in english: ")
+        if(letter_abe=="pi"):
+            print("correct")
+            good+=1
+            vec_abe[15]="P"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [Q] in english: ")
+        if(letter_abe=="kiu"):
+            print("correct")
+            good+=1
+            vec_abe[16]="Q"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [R] in english: ")
+        if(letter_abe=="ar"):
+            print("correct")
+            good+=1
+            vec_abe[17]="R"
+        else:
+            print("incorrect")
+        
+        letter_abe=input("write the letter [S] in english: ")
+        if(letter_abe=="es"):
+            print("correct")
+            good+=1
+            vec_abe[18]="S"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [T] in english: ")
+        if(letter_abe=="ti"):
+            print("correct")
+            good+=1
+            vec_abe[19]="T"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [U] in english: ")
+        if(letter_abe=="iu"):
+            print("correct")
+            good+=1
+            vec_abe[20]="U"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [V] in english: ")
+        if(letter_abe=="vi"):
+            print("correct")
+            good+=1
+            vec_abe[21]="V"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [W] in english: ")
+        if(letter_abe=="dabliu"):
+            print("correct")
+            good+=1
+            vec_abe[22]="W"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [X] in english: ")
+        if(letter_abe=="ex"):
+            print("correct")
+            good+=1
+            vec_abe[23]="X"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [Y] in english: ")
+        if(letter_abe=="uai"):
+            print("correct")
+            good+=1
+            vec_abe[24]="Y"
+        else:
+            print("incorrect")
+
+        letter_abe=input("write the letter [Z] in english: ")
+        if(letter_abe=="set"):
+            print("correct")
+            good+=1
+            vec_abe[25]="Z"
+        else:
+            print("incorrect")
+        
+        print("Answers:")
+        recorrer_lista_abecedary(vec_abe)
+        print(f"result: {good}/26")
+        print()
+        print("¿you want to see the list of answers, but if you see them the test is over?yes/not")
+        see_ans=input()
+        if(see_ans=="yes"):
+            list_correct_abecedary()
+            exit="yes"
+        elif(see_ans=="not"):
+            print()            
+            print("¿you want to leave the abecedary test yes/not?")
+            see_ans=input()
+            if(see_ans=="yes"):
+                see_ans="yes"
+            elif(see_ans=="not"):
+                see_ans="not"
+
---
+++ words.py
@@ -0,0 +1,167 @@
+#funciones
+def recorrer_lista_test(list_test):
+    print("correct words")
+    for x in list_test:
+        print(x)
+
+def correct_list_test_1():
+    print('lista correcta del test 1')
+    print('spanish--english--pronunciation')
+    print('común--common--kamen')
+    print('signifigar/medi@--mean--min')
+    print('arrepentid@--regretted--regreded')
+    print('quedate--stay--stey')
+    print('lanzar--throw--trou')
+    print('mediante--through--truu')
+    print('más alla--beyond--bi-ant')
+    print('cubrir--cover--coverd')
+    print('tonelada--ton--ton')
+    print('agoviado--overwhelmed--over-welt')
+
+
+def test1():
+    list_test_1=["---------","---------","---------","---------","---------","---------","---------","---------","---------","---------"]
+    good=0
+    exit="not"
+    while (good<10 and exit=="not"):
+        print("write común in english")
+        comun_word=input()
+        print("write the pronunciation of común in english")
+        comun_pronunciation=input()
+        if(comun_word=="common" and comun_pronunciation=="kamen"):
+            print("correct")
+            good=good+1
+            list_test_1[0]="Común"            
+        else:
+                print("incorrect")
+        print('')
+        
+        print('write significar/medi@ in english')
+        significar_word=input()
+        print('write the pronunciation of significar/medi@ in english')
+        significar_pronunciation=input()
+        if(significar_word=='mean' and significar_pronunciation=='min'):
+            print('correct')
+            good=good+1
+            list_test_1[1]="significar"
+        else:
+            print('incorrect')
+        print('')
+
+        print('write arrepentid@ in english')
+        regretted_word=input()
+        print('write the pronunciation of arrepentid@ in english')
+        regretted_pronunciation=input()
+        if(regretted_word=='regretted' and regretted_pronunciation=='regreded'):
+            print('correct')
+            good=good+1
+            list_test_1[2]="arrepentid@"
+        else:
+            print('incorrect')
+        print('')
+        
+        print('write quedate in english')
+        stay_word=input()
+        print('write the pronunciation of quedate in english')
+        stay_pronunciation=input()
+        if(stay_word=='stay' and stay_pronunciation=='stey'):
+            print('correct')
+            good=good+1
+            list_test_1[3]="quedate"
+        else:
+            print('incorrect')
+        print('')
+
+        print('write tirar in english')
+        throw_word=input()
+        print('write the pronunciation of tirar in english')
+        throw_pronunciation=input()
+        if(throw_word=='throw' and throw_pronunciation=='trou'):
+            print('correct')
+            good=good+1
+            list_test_1[4]="tirar"
+        else:
+            print('incorrect')
+        print('')
+
+        print('write mediante in english')
+        through_word=input()
+        print('write the pronunciation of mediante in english')
+        through_pronunciation=input()
+        if(through_word=='through' and through_pronunciation=='truu'):
+            print('correct')
+            good=good+1
+            list_test_1[5]="mediante"
+        else:
+            print('incorrect')
+        print('')
+
+        print('write más alla in english')
+        beyond_word=input()
+        print('write the pronunciation of más alla in english')
+        beyond_pronunciation=input()
+        if(beyond_word=='beyond' and beyond_pronunciation=='bi-ant'):
+            print('correct')
+            good=good+1
+            list_test_1[6]="más alla"
+        else:
+            print('incorrect')
+        print('')
+
+        print('write cubrir in english')
+        cover_word=input()
+        print('write the pronunciation of cubrir in english')
+        cover_pronunciation=input()
+        if(cover_word=='cover' and cover_pronunciation=='coverd'):
+            print('correct')
+            good=good+1
+            list_test_1[7]="cubrir"
+        else:
+            print('incorrect')
+        print('')
+
+        print('write tonelada in english')
+        ton_word=input()
+        print('write the pronunciation of tonelada in english')
+        ton_pronunciation=input()
+        if(ton_word=='ton' and ton_pronunciation=='ton'):
+            print('correct')
+            good=good+1
+            list_test_1[8]="tonelada"
+        else:
+            print('incorrect')
+        print('')
+
+        print('write agoviado in english')
+        overwhelmed_word=input()
+        print('write the pronunciation of agoviado in english')
+        overwhelmed_pronunciation=input()
+        if(overwhelmed_word=='overwhelmed' and overwhelmed_pronunciation=='over-welt'):
+            print('correct')
+            good=good+1
+            list_test_1[9]="agoviado"
+        else:
+            print('incorrect')
+        print('')
+
+        recorrer_lista_test(list_test_1)
+        print(f"{good}/10")
+                
+        print('')   
+        print("¿you want to leave the test#1 yes/not?")
+        exit=input()
+        
+        if(exit=='not'):
+            list_test_1=["---------","---------","---------","---------","---------","---------","---------","---------","---------","---------"]
+            good=0            
+
+        print("¿you want to see the list of answers, but if you see them the test is over?yes/not")
+        see_answers_test_1=input()
+        if(see_answers_test_1=="yes"):
+            correct_list_test_1()
+            exit="yes"
+        
+        print("")
+
+    return 1
+
