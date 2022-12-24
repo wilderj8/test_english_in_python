7f9d71acbb0bf2dd9c3eff2e04de0d53de5e42cc
wilderj8 <90732363+wilderj8@users.noreply.github.com>
2022-12-24 13:27:58 -0500

    24/dic/2022

--- theory.py
+++ theory.py
@@ -196,6 +196,65 @@ def list_correct_pronouns():
     print("ellos se encontraron en serios problemas--they found themselves in serious trouble")
     print("como ustedes mismos pueden ver, esto ha sido muy fácil--as you yourselves can see, this has been very easy")
 
+def list_correct_adjective():
+    print("what is a adjective--an adjective is a word that describes the traits,qualities or number of a noun")
+    print()
+    print("words")
+    print("angry--enojado")
+    print("happy--feliz")
+    print("sad--triste")
+    print("thoughtful--considerado/atento")
+    print("arrogant--creído")
+    print("ambitious--ambicioso")
+    print("smooth--liso")
+    print("bumpy--irregular//con baches/con hoyos")
+    print("soft--blando/suave")
+    print("spicy--picante")
+    print("sweet--dulce")
+    print("sour--agrio/ácido")
+    print("remaining--restante")
+    print("lightwight--liviano/ligero/peso ligero/de poco peso/persona que se emborracha fácilmente/pelagatos/don nadie")
+    print("wanting--deficiente/insuficiente")
+    print("spent--gastado/vacío/usado")
+    print("able--poder/capaz/talenotos(a)")
+    print("silly--tonto(a)/bobo(a)/absurdo(a)")
+    print("funky--en la onda/en la moda/apestosó(a)/fétido(a)")
+    print("halfway--por la mitad/a medias")
+    print("further--más lejos/más lejano/más extenso/más a fondo")    
+    print("enough--suficiente/bastante/")
+    print("feasible--viable/factible/probable")
+    print("bold--vailiente/audaz/atrevido/")
+    print("paid--pagado/pago(a)/remunerado")
+    print("nested--anidado/dentro de una cosa/alojado")
+    print("tricky--complicado(a)/delicado(a)/que tiene su maña/difícil/complicado(a)/tramposo(a)/falso(a)")
+    print("fixed--fijo(a)/inamovible/inalterable/reparado/areglado(a)/")    
+    print("desired--deseado/anhelado/apropiado(a)")
+    print("overlapping--superpuesto/solapado(a)")
+    print("pretty--bonito(a)/lindo(a)/precioso(a)/guapo(a)")
+    print("narrow--angosto/estrecho/corto/reducido")    
+    print("accurate--preciso(a)/exato(a)/certero(a)/riguroso(a)/acertado(a)")
+    print("meaningful--significativo(a)")
+    print("deep--profundo/hondo/grave")
+    print("unexpected--inesperado(a)/imprevisto(a)")
+    print("related--relacionado/familiar/pariente")
+    print("entire--entero(a)/todo(a)")    
+    print("several--varios/varias")
+    print("readable--ameno(a)/entrenido(a)/legible")
+    print("another--otro/distinto")
+    print("whole--entero/total/todo/completo")    
+    print("complex--complicado/complejo")
+    print("those--aquellos/esos/esas")
+    print("shortest--mas bajo/más corto")
+    print("subtle--sutil/disimulado/desimulada")
+    print("remaining--restante")
+    print()
+    print("examples")
+    print("red car--carro rojo")
+    print("red cars--carros rojos")
+    print("gata--female cat")
+    print("gato--male cat")
+    print("i like the green car--me gusta el carro verde")
+
 def list_correct_basic_sentence_strcture():
     print()
     print("question---answer")
@@ -207,6 +266,8 @@ def list_correct_basic_sentence_strcture():
     correct_list_test_noun_subject()
     print("what are pronouns?--pronouns are the words we often use to talk about a person when we are not using their name")
     list_correct_pronouns()
+    print()
+    list_correct_adjective()
     
 def list_correct_months():
     print()
@@ -1714,6 +1775,534 @@ def pronouns():
     print()
     return point
 
+def adjective():
+    point=False
+    list_test_adjective=["------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------""------","------","------","------","------","------","------","------","------","------","------","------""------","------","------","------","------","------","------","------","------","------","------","------","------"]
+    good=0
+    print('write enfadado in english')
+    angry_word=input()    
+    if(angry_word=='angry'):
+        print('correct')
+        good=good+1
+        list_test_adjective[0]='angry'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write feliz in english')
+    happy_word=input()
+    if(happy_word=='happy'):
+        print('correct')
+        good=good+1
+        list_test_adjective[1]='happy'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write triste in english')
+    sad_word=input()
+    if(sad_word=='sad'):
+        print('correct')
+        good=good+1
+        list_test_adjective[2]='sad'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write considerado/atento in english')
+    thoughtful_word=input()
+    if(thoughtful_word=='thoughtful'):
+        print('correct')
+        good=good+1
+        list_test_adjective[3]='thoughtful'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write creído in english')
+    arrogant_word=input()
+    if(arrogant_word=='arrogant'):
+        print('correct')
+        good=good+1
+        list_test_adjective[4]='arrogant'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write ambicioso in english')
+    ambitious_word=input()
+    if(ambitious_word=='ambitious'):
+        print('correct')
+        good=good+1
+        list_test_adjective[5]='ambitious'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write liso in english')
+    smooth_word=input()
+    if(smooth_word=='smooth'):
+        print('correct')
+        good=good+1
+        list_test_adjective[6]='smooth'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write irregular/con baches/con hoyos in english')
+    bumpy_word=input()
+    if(bumpy_word=='bumpy'):
+        print('correct')
+        good=good+1
+        list_test_adjective[7]='bumpy'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write blando/suave in english')
+    soft_word=input()
+    if(soft_word=='soft'):
+        print('correct')
+        good=good+1
+        list_test_adjective[8]='soft'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write picante in english')
+    spicy_word=input()
+    if(spicy_word=='spicy'):
+        print('correct')
+        good=good+1
+        list_test_adjective[9]='spicy'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write dulce in english')
+    sweet_word=input()
+    if(sweet_word=='sweet'):
+        print('correct')
+        good=good+1
+        list_test_adjective[10]='sweet'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write agrio/ácido in english')
+    sour_word=input()
+    if(sour_word=='sour'):
+        print('correct')
+        good=good+1
+        list_test_adjective[11]='sour'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write restante in english')
+    remaining_word=input()
+    if(remaining_word=='remaining'):
+        print('correct')
+        good=good+1
+        list_test_adjective[12]='remaining'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write liviano/ligero/peso ligero/de poco peso/persona que se emborracha fácilmente/pelagatos/don nadie in english')
+    lightwight_word=input()
+    if(lightwight_word=='lightwight'):
+        print('correct')
+        good=good+1
+        list_test_adjective[13]='lightwight'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write deficiente/insuficiente in english')
+    wanting_word=input()
+    if(wanting_word=='wanting'):
+        print('correct')
+        good=good+1
+        list_test_adjective[14]='wanting'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write gastado/usado/vacío in english')
+    spent_word=input()
+    if(spent_word=='spent'):
+        print('correct')
+        good=good+1
+        list_test_adjective[15]='spent'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write poder/capaz/talentoso(a) in english')
+    able_word=input()
+    if(able_word=='able'):
+        print('correct')
+        good=good+1
+        list_test_adjective[16]='able'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write tonto(a)/bobo(a)/absurdo(a) in english')
+    silly_word=input()
+    if(silly_word=='silly'):
+        print('correct')
+        good=good+1
+        list_test_adjective[17]='silly'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write en la onda/en la moda/apestosó(a)/fétido(a) in english')
+    funky_word=input()
+    if(funky_word=='funky'):
+        print('correct')
+        good=good+1
+        list_test_adjective[18]='funky'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write por la mitad/a medias in english')
+    halfway_word=input()
+    if(halfway_word=='halfway'):
+        print('correct')
+        good=good+1
+        list_test_adjective[19]='halfway'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write mas lejos/más lejano/más extenso/más a fondo in english')
+    further_word=input()
+    if(further_word=='further'):
+        print('correct')
+        good=good+1
+        list_test_adjective[20]='further'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write suficiente/bastante in english')
+    enough_word=input()
+    if(enough_word=='enough'):
+        print('correct')
+        good=good+1
+        list_test_adjective[21]='enough'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write viable/factible/probable in english')
+    feasible_word=input()
+    if(feasible_word=='feasible'):
+        print('correct')
+        good=good+1
+        list_test_adjective[22]='feasible'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write valiente/audaz/atrevido in english')
+    bold_word=input()
+    if(bold_word=='bold'):
+        print('correct')
+        good=good+1
+        list_test_adjective[23]='bold'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write pagado/pago(a)/remunerado in english')
+    paid_word=input()
+    if(paid_word=='paid'):
+        print('correct')
+        good=good+1
+        list_test_adjective[24]='paid'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write anidado/dentro de una cosa/alojado in english')
+    nested_word=input()
+    if(nested_word=='nested'):
+        print('correct')
+        good=good+1
+        list_test_adjective[25]='nested'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write complicado(a)/delicado(a)/que tiene su maña/difícil/complicado(a)/tramposo(a)/falso(a) in english')
+    tricky_word=input()
+    if(tricky_word=='tricky'):
+        print('correct')
+        good=good+1
+        list_test_adjective[26]='tricky'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write fijo(a)/inamovible/inalterable/reparado/areglado(a) in english')
+    fixed_word=input()
+    if(fixed_word=='fixed'):
+        print('correct')
+        good=good+1
+        list_test_adjective[27]='fixed'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write deseado/anhelado/apropiado(a) in english')
+    desired_word=input()
+    if(desired_word=='desired'):
+        print('correct')
+        good=good+1
+        list_test_adjective[28]='desired'
+    else:
+        print('incorrect')
+    print('')   
+
+    print('write superpuesto/solapado(a) in english')
+    overlapping_word=input()
+    if(overlapping_word=='overlapping'):
+        print('correct')
+        good=good+1
+        list_test_adjective[29]='overlapping'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write bonito(a)/lindo(a)/precioso(a)/guapo(a) in english')
+    pretty_word=input()
+    if(pretty_word=='pretty'):
+        print('correct')
+        good=good+1
+        list_test_adjective[30]='pretty'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write angosto/estrecho/corto/reducido in english')
+    narrow_word=input()
+    if(narrow_word=='narrow'):
+        print('correct')
+        good=good+1
+        list_test_adjective[31]='narrow'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write preciso(a)/exacto(a)/certero(a)/riguroso(a)/acertado(a) in english')
+    accurate_word=input()
+    if(accurate_word=='accurate'):
+        print('correct')
+        good=good+1
+        list_test_adjective[32]='accurate'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write significativo(a) in english')
+    meaningful_word=input()
+    if(meaningful_word=='meaningful'):
+        print('correct')
+        good=good+1
+        list_test_adjective[33]='meaningful'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write profundo/hondo/grave in english')
+    deep_word=input()
+    if(deep_word=='deep'):
+        print('correct')
+        good=good+1
+        list_test_adjective[34]='deep'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write inesperado(a)/imprevisto(a) in english')
+    unexpected_word=input()
+    if(unexpected_word=='unexpected'):
+        print('correct')
+        good=good+1
+        list_test_adjective[35]='unexpected'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write relacionado/familiar/pariente in english')
+    related_word=input()
+    if(related_word=='related'):
+        print('correct')
+        good=good+1
+        list_test_adjective[36]='related'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write entero(a)/todo(a) in english')
+    entire_word=input()
+    if(entire_word=='entire'):
+        print('correct')
+        good=good+1
+        list_test_adjective[37]='entire'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write varios/varias in english')
+    several_word=input()
+    if(several_word=='several'):
+        print('correct')
+        good=good+1
+        list_test_adjective[38]='several'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write ameno(a)/entretenido(a)/legible in english')
+    readable_word=input()
+    if(readable_word=='readable'):
+        print('correct')
+        good=good+1
+        list_test_adjective[39]='readable'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write otro/distinto in english')
+    another_word=input()
+    if(another_word=='another'):
+        print('correct')
+        good=good+1
+        list_test_adjective[40]='another'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write entero/total/todo/completo in english')
+    whole_word=input()
+    if(whole_word=='whole'):
+        print('correct')
+        good=good+1
+        list_test_adjective[41]='whole'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write complicado/complejo in english')
+    complex_word=input()
+    if(complex_word=='complex'):
+        print('correct')
+        good=good+1
+        list_test_adjective[42]='complex'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write aquellos/esas/esos in english')
+    those_word=input()
+    if(those_word=='those'):
+        print('correct')
+        good=good+1
+        list_test_adjective[43]='those'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write más bajo/más corto in english')
+    shortest_word=input()
+    if(shortest_word=='shortest'):
+        print('correct')
+        good=good+1
+        list_test_adjective[44]='shortest'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write sutil/disimulado/desimulada in english')
+    subtle_word=input()
+    if(subtle_word=='subtle'):
+        print('correct')
+        good=good+1
+        list_test_adjective[45]='subtle'
+    else:
+        print('incorrect')
+    print('')
+
+    print('write restante in english')
+    remaining_word=input()
+    if(remaining_word=='remaining'):
+        print('correct')
+        good=good+1
+        list_test_adjective[46]='remaining'
+    else:
+        print('incorrect')
+    print('')
+
+    if(good==45):
+        point=True 
+        print("congratulations")        
+    else:
+        print("you not have point")
+
+    return point
+
+def adjective_example():
+    point=False
+    good=0
+    ans_adj_exa_1=input("write in english [carro rojo]: ")
+    if(ans_adj_exa_1=="red car"):
+        print("correct")
+        good+=1
+    else:
+        print("incorrect")
+
+    ans_adj_exa_2=input("write in english [carros rojos]: ")
+    if(ans_adj_exa_2=="red cars"):
+        print("correct")
+        good+=1
+    else:
+        print("incorrect")
+
+    ans_adj_exa_3=input("write in english [gata]: ")
+    if(ans_adj_exa_3=="female cat"):
+        print("correct")
+        good+=1
+    else:
+        print("incorrect")
+
+    ans_adj_exa_4=input("write in english [gato]: ")
+    if(ans_adj_exa_4=="male cat"):
+        print("correct")
+        good+=1
+    else:
+        print("incorrect")
+
+    ans_adj_exa_5=input("write in english [me gusta el carro verde]: ")
+    if(ans_adj_exa_5=="i like the green car"):
+        print("correct")
+        good+=1
+    else:
+        print("incorrect")
+
+    if(good==5):
+        print("congratulations")
+        point=True
+    else:
+        print("error 404")
+
+    return point
+
 def simple_rules():
     vec_sim_rul=["------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------","------"]
     good_sim_rul=0
@@ -1899,7 +2488,7 @@ def simple_rules():
 def basic_sentence_structure():
     print()
     print("BASIC SENTENCE STRUCTURE")
-    vec_bas_sen_str=["------","------","------","------","------","------"]
+    vec_bas_sen_str=["------","------","------","------","------","------","------","------","------"]
     good_bas_sen_str=0
     answer_bas_sen_str=input("what the basic sentence structure?: ")
     if(answer_bas_sen_str=="subject+verb+object"):
@@ -1949,6 +2538,28 @@ def basic_sentence_structure():
     if(pronouns_var):        
         good_bas_sen_str+=1
         vec_bas_sen_str[5]="Pronouns"        
+
+    print()
+    answer_wh_adj=input("what is a adjective: ")
+    if(answer_wh_adj=="an adjective is a word that describes the traits,qualities or number of a noun"):
+        print("correct")
+        good_bas_sen_str+=1
+        vec_bas_sen_str[6]="what is a adjective"
+    else:
+        print("incorrect")
+    print()
+        
+    point_adjective=adjective()
+    if(point_adjective):
+        good_bas_sen_str+=1
+        vec_bas_sen_str[7]="adjective words"
+    print()
+    
+
+    point_adjective_example=adjective_example()
+    if(point_adjective_example):
+        good_bas_sen_str+=1
+        vec_bas_sen_str[8]="example adjective"
 
     print()    
     print("Answers:")
